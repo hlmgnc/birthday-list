3c7b574adab6fe52bf5c37674146e9467d3c10d5
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-11-10 17:26:13 +0100

    Add some buttons

--- src/List.js
+++ src/List.js
@@ -1,13 +1,20 @@
 import React from 'react'
-
 const List = ({ people }) => {
+  const clickHandler = (id) => {
+    people.filter((person) => person.id !== id)
+    return people
+  }
   return (
     <>
       {people.map((person) => {
         const { id, name, image, age } = person
+
         return (
           <div key={id} className="person">
             <img src={image} alt={name} />
+            <button className="btn" onClick={clickHandler}>
+              Remove
+            </button>
             <h4>
               {name}
               <p> {age} years old </p>
